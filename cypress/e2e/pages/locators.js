89a6cfa8f54b73f5af5db39f0a33152fafd10fae
Syeda <syeda.quratulain@monese.com>
2023-02-04 17:16:31 +0200

    Locator added

--- cypress/e2e/pages/locators.js
+++ cypress/e2e/pages/locators.js
@@ -5,8 +5,7 @@ class locators {
   save_settings_btn = "button[class^='Button_Button__'] span";
   sign_up_btn = ".styles_SignUpButton__pw0kf span";
   country_selection_dropdown = "div[class^='styles_SelectedItem'] span";
-  search_within_country_dropdown =
-    "div[class^='styles_SearchBoxContainer'] input";
+  search_within_country_dropdown = "div[class^='styles_SearchBoxContainer'] input";
   click_searched_country = "div[class^='styles_ListItem'] span";
   click_continue_btn = "button[class^='Button_Button'] span"
   homebanner_sign_up_btn = "div[class^='SignupIcons_SignupIconsContainer'] div";
